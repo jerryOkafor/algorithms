f9c5f29838ad99a596c9f5ebff7b9a073f19edf6
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-08-21 09:53:59 +0100

    refactor : typo

--- javascript/binarySearch/test/test.js
+++ javascript/binarySearch/test/test.js
@@ -4,29 +4,20 @@ var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
     41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 describe("Test binarySearch", () => {
-    it("Shoult return 20", () => {
+    it("Should return 20", () => {
         expect(doSearch(primes, 73)).toBe(20)
     })
 
-    it("Shoult return 19", () => {
+    it("Should return 19", () => {
         expect(doSearch(primes, 71)).toBe(19)
     })
 
-    it("Shoult return 9", () => {
+    it("Should return 9", () => {
         expect(doSearch(primes, 29)).toBe(9)
     })
 
-    it("Shoult return -1", () => {
+    it("Should return -1", () => {
         expect(doSearch(primes, 100)).toBe(-1)
     })
 
 })
-
-
-
-// var result = doSearch(primes, 73);
-// console.log("Found prime at index " + result);
-
-// Program.assertEqual(doSearch(primes, 73), 20);
-// Program.assertEqual(doSearch(primes, 71), 19);
-// Program.assertEqual(doSearch(primes, 29), 9);
