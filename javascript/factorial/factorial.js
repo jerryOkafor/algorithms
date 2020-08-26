4c26469ca821417b03e7542b62285c5f651c834d
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-08-26 14:48:00 +0100

    feat : add recursive factorial with tests.

--- javascript/factorial/factorial.js
+++ javascript/factorial/factorial.js
@@ -1,4 +1,7 @@
 const factorial = (n) => {
+    //preconditons
+    if (isNaN(n)) throw 'not a number'
+    if (n < 0) throw 'not a positive number'
     //base
     if (n === 0) { return 1 }
 
--- javascript/factorial/factorial.test.js
+++ javascript/factorial/factorial.test.js
@@ -6,12 +6,24 @@ describe("Test Recursive Facotrial",()=>{
         expect(factorial(0)).toBe(1)
     })
 
+    it("Should throw",()=>{
+        expect(() => { factorial("a") }).toThrowError('not a number')
+    })
+
+    it("Should throw", () => {
+        expect(() => { factorial(-1) }).toThrowError('not a positive number')
+    })
+
     it("Should return 120", () => {
         expect(factorial(5)).toBe(120)
     })
 
     it("Should return 24", () => {
         expect(factorial(4)).toBe(24)
+    })
+
+    it("Should return 8", () => {
+        expect(factorial(8)).toBe(40320)
     })
 
     it("Should return 8", () => {
