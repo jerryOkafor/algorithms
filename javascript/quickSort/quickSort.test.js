57365666cc78b3eeacf8810efd5192fbd9d6a6c5
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-09-04 07:37:09 +0100

    feat : add more tests

--- javascript/quickSort/quickSort.test.js
+++ javascript/quickSort/quickSort.test.js
@@ -8,9 +8,25 @@ describe("Test Partition", () => {
         expect(partition(array, 0, array.length - 1)).toBe(4)
         expect(array).toEqual([5, 2, 3, 4, 6, 7, 14, 9, 10, 11, 12])
     })
-    
+
     it("Should return : [-1, 0, 5, 2, 3, 4, 5, 12, 10, 11, 14]", () => {
         expect(partition(array2, 0, array2.length - 1)).toBe(6)
         expect(array2).toEqual([-1, 0, 5, 2, 3, 4, 5, 12, 10, 11, 14])
     })
+})
+
+var array3 = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+var array4 = [-1, -10, 5, 0, 12, 20, 14, 3, 101, 600]
+
+describe("Test Quicksort", () => {
+    it("Should return [2, 3, 5, 6, 7, 9, 10, 11, 12, 14]", () => {
+        sortWithQuickSort(array3)
+        expect(array3).toEqual([2, 3, 5, 6, 7, 9, 10, 11, 12, 14])
+    })
+
+    it("Should return [-10, -1, 0, 3, 5, 12, 14, 20, 101, 600]", () => {
+        sortWithQuickSort(array4)
+        expect(array4).toEqual([-10, -1, 0, 3, 5, 12, 14, 20, 101, 600])
+    })
+
 })
